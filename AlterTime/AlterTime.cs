437810925560f80d97f743812bbb2c23f84378fe
Noah Pilarski <megapiggy@outerwildsmods.com>
2024-06-06 07:19:07 -0400

    Added 10x speed option.

--- AlterTime/AlterTime.cs
+++ AlterTime/AlterTime.cs
@@ -10,16 +10,17 @@ public class AlterTime : ModBehaviour
 {
 	public Dictionary<(Key key, Key numpadKey), int> timescales = new()
 	{
-		{ (Key.Digit0,  Key.Numpad0), 0 },
-		{ (Key.Digit1,  Key.Numpad1), 1 },
-		{ (Key.Digit2,  Key.Numpad2), 2 },
-		{ (Key.Digit3,  Key.Numpad3), 3 },
-		{ (Key.Digit4,  Key.Numpad4), 4 },
-  		{ (Key.Digit5,  Key.Numpad5), 5 },
-    		{ (Key.Digit6,  Key.Numpad6), 6 },
-    		{ (Key.Digit7,  Key.Numpad7), 7 },
-      		{ (Key.Digit8,  Key.Numpad8), 8 },
-		{ (Key.Digit9,  Key.Numpad9), 9 }
+		{ (Key.Digit0, Key.Numpad0), 0 },
+		{ (Key.Digit1, Key.Numpad1), 1 },
+		{ (Key.Digit2, Key.Numpad2), 2 },
+		{ (Key.Digit3, Key.Numpad3), 3 },
+		{ (Key.Digit4, Key.Numpad4), 4 },
+  		{ (Key.Digit5, Key.Numpad5), 5 },
+		{ (Key.Digit6, Key.Numpad6), 6 },
+		{ (Key.Digit7, Key.Numpad7), 7 },
+		{ (Key.Digit8, Key.Numpad8), 8 },
+		{ (Key.Digit9, Key.Numpad9), 9 },
+		{ (Key.Minus, Key.NumpadMinus), 10 }
 	};
 
 	public void Start()
